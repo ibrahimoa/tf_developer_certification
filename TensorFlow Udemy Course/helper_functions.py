78c051edd4f45912d802617f1edc0f81fbc82ca3
ibrahim.oulad <ibrahim.oulad@valid.com>
2021-07-28 10:29:49 +0200

    fix: now the unzip_data functions unzips the file in the same folder as the .zip file.

--- TensorFlow Udemy Course/helper_functions.py
+++ TensorFlow Udemy Course/helper_functions.py
@@ -10,7 +10,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import itertools
 import tensorflow as tf
-
+import os
 # Create a function to import an image and resize it to be able to be used with our model
 
 
@@ -254,13 +254,13 @@ def compare_historys(original_history, new_history, initial_epochs=5):
 
 def unzip_data(filename):
     """
-    Unzips filename into the current working directory.
+    Unzips filename into his directory.
 
     Args:
       filename (str): a filepath to a target zip folder to be unzipped.
     """
     zip_ref = zipfile.ZipFile(filename, "r")
-    zip_ref.extractall()
+    zip_ref.extractall(os.path.dirname(filename))
     zip_ref.close()
 
 
